6bbf092e1ed5a5226a440b4230dbd7d59e1caf00
Your Name <minajmiha02@gmail.com>
2025-04-24 09:30:20 +0600

    function

---
+++ first-typescript/index.js
@@ -0,0 +1,6 @@
+var username = "Arifa Islam Asma";
+console.log(username);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(10, 20);
---
+++ first-typescript/index.ts
@@ -0,0 +1,6 @@
+let username = "Arifa Islam Asma";
+console.log(username);
+function addNumbers (num1:number,num2:number) {
+    console.log(num1+num2)
+}
+addNumbers(10,20)
