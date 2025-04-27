21da3d42f39ddb8a626fea6cfa35b4be955e6293
Your Name <minajmiha02@gmail.com>
2025-04-27 18:44:16 +0600

    object-type

---
+++ user-defined-type/object-type.js
@@ -0,0 +1,31 @@
+var users;
+users = [];
+var user1;
+user1 = { userName: "Arifa", userId: 101, };
+users.push(user1);
+console.log(users);
+var user2;
+user2 = { userName: "Yeasmin", userId: 102, };
+users.push(user2);
+console.log(users);
+var user3;
+user3 = { userName: "Minaj", userId: 103, };
+users.push(user3);
+console.log(users);
+var user4;
+user4 = { userName: "Anika", userId: 104, };
+users.push(user4);
+console.log(users);
+var user5;
+user5 = { userName: "Ajmi", userId: 105, };
+users.push(user5);
+console.log(users);
+var getRequest;
+getRequest = "GET";
+function requestHanndler(requestType) {
+    console.log(requestType);
+}
+requestHanndler("GET");
+// let user:object;
+// user={arifa:"arifa"};
+// jekono poristhitite nijeke maniye neoa,nijer moto kore jibon sajano,nijei nijeke carry korte para.kokhon venge na pora. karo khoti na kora.santite nijer moto kore baca.ekjon valo sontan hote cai,adorsho ma hote cai.attonirvorshil hote caiekjon adorsho bou hote cai ami.ami kokhon kono kichu emon vabe cai ni jeta petei hobe , na pele nai hobe.life ja ache tai niye sontusto thakte cai,aro kichu pete ami try korbo to obssoi,allah jodi seta amar jonno valo mone koren tobe diben ,ar ami sukriya aday korbo,ar na dileo alhamdulillah.
