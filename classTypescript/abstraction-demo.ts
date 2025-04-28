d60cd407ae47354b5d7a380274d68316b5780596
Your Name <minajmiha02@gmail.com>
2025-04-28 15:30:37 +0600

    abstraction-demo

--- classTypescript/abstraction-demo.ts
+++ classTypescript/abstraction-demo.ts
@@ -1,26 +1,21 @@
-class User {
+abstract class User {
     userName: string;
     age: number;
     constructor(userName: string, age: number) {
       this.userName = userName;
       this.age = age;
     }
-    display(): void {
-      console.log(`username:${this.userName},age${this.age}`);
-    }
+ abstract   display(): void 
   }
-  class Student extends User {
+ abstract class Student extends User {
     studentId: number;
     constructor(userName: string, age: number, studentId: number) {
       super(userName, age);
       this.studentId = studentId;
     }
-    display(): void {
-      console.log(`username:${this.userName},age${this.age},id:${this.studentId}`);
-    }
+ abstract   display(): void 
   }
-  let student1 = new Student("Keya", 25, 19277);
-  student1.display();
-  let user1 = new User("arifa islam", 20);
-  user1.display();
-  
+//   let student1 = new Student("Keya", 25, 19277);
+//   student1.display();
+//   let user1 = new User("arifa islam", 20);
+//   user1.display(); 
