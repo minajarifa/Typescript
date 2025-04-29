397437963a96736aa085cf66128c049466fb01b1
Your Name <minajmiha02@gmail.com>
2025-04-29 07:18:18 +0600

    userClass

--- moduol-demo/index.js
+++ moduol-demo/index.js
@@ -1,2 +1,5 @@
-var user1 = new User("arifa islam", 20);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var UserClass_1 = require("./UserClass");
+var user1 = new UserClass_1.User("arifa islam", 20);
 user1.display();
--- moduol-demo/index.ts
+++ moduol-demo/index.ts
@@ -1,3 +1,3 @@
-
+import { User } from "./UserClass";
 let user1 = new User("arifa islam", 20);
 user1.display();
